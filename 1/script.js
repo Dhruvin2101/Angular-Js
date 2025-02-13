945c8be02f4d010d753186b18136a1b35674c89b
Dhruvin Patel <149464919+Dhruvin2101@users.noreply.github.com>
2025-02-14 00:52:41 +0530

    Changes

--- 1/script.js
+++ 1/script.js
@@ -1,8 +1,20 @@
 let bg = document.getElementById("main");
 let input = document.getElementById("color_name");
+let go = document.getElementById("go");
+let number_input = document.getElementById("number_input");
 let submit = document.getElementById("submit");
 
-submit.addEventListener("click", function(event){
+go.addEventListener("click", function(event){
     event.preventDefault();
         bg.style.backgroundColor = input.value;
+})
+
+number_input.addEventListener("input", function(event){
+    event.preventDefault();
+    if(Number(number_input.value.length) === 10){
+        bg.style.backgroundColor = "green";
+    }
+    else{
+        bg.style.backgroundColor = "red";
+    }
 })
